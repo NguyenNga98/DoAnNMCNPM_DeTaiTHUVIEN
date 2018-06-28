9d2e624cf594359ec9c9c0ccc6c242469561640f
TienVu <14520954@gm.uit.edu.vn>
2018-06-28 11:00:23 +0700

    add model ignore

---
+++ QuanLyThuVien_NTMT/Cross/ViewModel/SachModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Cross.ViewModel
+{
+    public class SachModel
+    {
+        public string Tensach { get; set; }
+        public string TacGia { get; set; }
+        public string NhaXuatBan { get; set; }
+        public int NamXuatBan { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime NgayNhapSach { get; set; }
+        public bool? TinhTrang { get; set; }
+        public int TheLoaiId { get; set; }
+    }
+    public class MuonSachModel
+    {
+        public int DocGiaId { get; set; }
+        //public int SachId { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime NgayMuon { get; set; }
+    }
+
+    public class TrasachModel
+    {
+        public int Id { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime NgayTra { get; set; }
+        public decimal? TienPhat { get; set; }
+    }
+}
---
+++ QuanLyThuVien_NTMT/Cross/ViewModel/TheLoaiModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cross.ViewModel
+{
+    public class TheLoaiModel
+    {
+        public string TenTheLoai { get; set; }
+    }
+}
